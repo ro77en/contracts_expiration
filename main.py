fdd620600aae9c94fbac8fd59a5544fb647bb4a4
ro77en <lucas.mdantas@icloud.com>
2024-01-30 16:21:03 -0300

    expired contracts filter

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ with open('./data/contracts.pkl', 'rb') as f:
     contracts_df = pickle.load(f)
 
 def fetch_contracts(contracts_df, search):
-    today = datetime.now().date()
+    # today = datetime.now().date()
+    # using contracts before 2015 for tests
+    before_2015_contracts = datetime.strptime('2015-01-01', '%Y-%m-%d').date()
     if search == 1:
-        print(today)
+        expired = contracts_df[ contracts_df['END_DATE'] < before_2015_contracts ]
+        print(expired)
 
 def send_email():
     pass
