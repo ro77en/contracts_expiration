b3dcb9a9f1e84a011a1a40324613bb0453fff30e
ro77en <lucas.mdantas@icloud.com>
2024-01-30 16:08:05 -0300

    building fetch contracts function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import pickle
 
@@ -5,4 +6,21 @@ import pickle
 with open('./data/contracts.pkl', 'rb') as f:
     contracts_df = pickle.load(f)
 
-print(contracts_df)
+def fetch_contracts(contracts_df, search):
+    today = datetime.now().date()
+    if search == 1:
+        print(today)
+
+def send_email():
+    pass
+
+def main():
+    print("""Search for:
+          1 - Expired contracts\n
+Type the corresponding number.""")
+    search = int(input())
+    fetch_contracts(contracts_df, search)
+
+if __name__ == '__main__':
+    main()
+
